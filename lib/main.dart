27d4dba5e4cf31e38e4d5ef12b6d43bbd4690b2d
kiran <kiran.sunway@gmail.com>
2022-06-02 17:02:15 +0545

    Simple API flutter

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Acer Demo'),
+    );
+  }
+}
+
+String? stringResponse;
+Map? mapResponse;
+Map? dataResponse;
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future apiCall() async {
+    http.Response response;
+    response = await http.get(Uri.parse("https://reqres.in/api/users/2"));
+    if (response.statusCode == 200) {
+      setState(() {
+        // stringResponse = response.body;
+        mapResponse = json.decode(response.body);
+        dataResponse = mapResponse!['data'];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    apiCall();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 300,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20), color: Colors.blue),
+          child: Center(
+            // child: Text(stringResponse.toString()),
+            child: dataResponse == null
+                ? const Text("Loading")
+                : Text(dataResponse!['first_name'].toString()),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
